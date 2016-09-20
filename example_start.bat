cdef5c3a97108c1198db7a61c8276412fa6a401f
MattChamberlain <Mattchamberlain2@gmail.com>
2016-09-20 11:32:00 -0400

    Generalized .bat

--- example_start.bat
+++ example_start.bat
@@ -1,2 +1,2 @@
-python main.py -u your@email.com -p yourpassword -s https://zybooks.zyante.com/#/zybook/UMICHEECS370Fall2016/chapter/3/section/4 -d "chromedriver.exe"
+python main.py -u your@email.com -p yourpassword -s https://zybooks.zyante.com/#/zybook/BOOKNAME/chapter/1/section/1 -d "chromedriver.exe"
 pause
